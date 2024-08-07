e16f3a067385693c61c27bf17c3a48ad1cebb25e
PavanKalyan <pavanklyn18@gmail.com>
2024-08-07 11:55:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <div>
+      <div className="user-search-input">
+        {/* Pills */}
+        {/* input field with search suggestions */}
+        <div>
+          <input type="text" />
+          {/* Search Suggestions */}
+        </div>
+      </div>
+   </div>
+  )
+}
+
+export default App
